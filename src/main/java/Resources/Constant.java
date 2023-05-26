2284fcf231e54fa91245c376ceacb2b1a8232ba3
PragatiK97 <pragatikatekhaye2021@gmail.com>
2023-05-26 16:05:44 +0530

    This is e-commerce project

---
+++ src/main/java/Resources/Constant.java
@@ -0,0 +1,49 @@
+package Resources;
+
+public class Constant {
+
+//MainMenu_PageObject:Search
+	public static String search="iphone";
+	public static String search1="samsung";
+	
+	public static String currencyoptionexpected="£ Pound Sterling";
+	public static String expectquesrysucces="Your enquiry has been successfully sent to the store owner!";
+	public static String loginsuccescheck="My Account";
+	public static String wishlistexpect="Success: You have added";
+	
+	
+//Header_PageObject:Phone
+	public static String phone_name="Pruthviraj";
+	public static String phone_email="Pruthviraj22@gmail.com";
+	public static String phone_enquiry="Please send us details about Iphone";
+	
+//Header_PageObject:Account
+	public static String account_email="monika.balwadkar@gmail.com";
+	public static String account_email1="kiyaan.khalde@gmail.com";
+	public static String account_password="monika";
+	public static String account_password1="kiyaan08";
+	
+//Header_PageObject:Shopping Cart
+	public static String shop_cart_qty="2";
+	
+//Header_PageObject:Checkout
+	public static String checkout_country="India";
+	public static String checkout_state="Maharashtra";
+	public static String checkout_pincode="411045";
+	
+//Product Comparison
+	public static String searchBox = "MacBook";	
+	public static String expectedErrMsg="Your shopping cart is empty!";
+	public static String expectedtextcomparison="Product Comparison";
+
+	
+//register case
+	public static String firstname="monica";
+	public static String lastname="balwadkar";
+	public static String phone="9028819090";
+	public static String password="123456789";
+	public static String password1="123456789";
+	public static String expectedtextregister="Congratulations! Your new account has been successfully created!";
+	
+	
+}
